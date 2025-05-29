4e68f859c762aa2873ef3289c15b039e6112f265
samiracode <samiratavakolli.k@gmail.com>
2025-05-29 20:41:53 +0200

    Fix API Gateway deployment and add stage resource

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -68,5 +68,12 @@ resource "aws_lambda_permission" "api_gateway" {
 resource "aws_api_gateway_deployment" "hello_deploy" {
   depends_on = [aws_api_gateway_integration.lambda_integration]
   rest_api_id = aws_api_gateway_rest_api.hello_api.id
-  stage_name  = "prod"
+  
 }
+
+resource "aws_api_gateway_stage" "prod" {
+  deployment_id = aws_api_gateway_deployment.hello_deploy.id
+  rest_api_id   = aws_api_gateway_rest_api.hello_api.id
+  stage_name    = "prod"
+}
+
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,6 +3,7 @@
 # Will contain outputs later (e.g., bucket name, URL, etc.)
 
 output "api_invoke_url" {
-  value = "https://${aws_api_gateway_rest_api.hello_api.id}.execute-api.eu-north-1.amazonaws.com/prod/hello"
+  value = "https://${aws_api_gateway_rest_api.hello_api.id}.execute-api.${var.aws_region}.amazonaws.com/prod/hello"
 }
+
 
